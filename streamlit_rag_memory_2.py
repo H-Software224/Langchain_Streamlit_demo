3547ad228c984069d37ab15978847926011a99c2
H-Software224 <han05280505@khu.ac.kr>
2025-07-20 18:06:21 +0900

    feat: update code and data

--- streamlit_rag_memory_2.py
+++ streamlit_rag_memory_2.py
@@ -57,7 +57,7 @@ def get_vectorstore(_docs):
 # PDF 문서 로드-벡터 DB 저장-검색기-히스토리 모두 합친 Chain 구축
 @st.cache_resource
 def initialize_components(selected_model):
-    file_path = "./data/korean_policy.pdf"
+    file_path = r"./data/졸업논문(소프트웨어융합)_강의계획서.pdf"
     pages = load_and_split_pdf(file_path)
     vectorstore = get_vectorstore(pages)
     retriever = vectorstore.as_retriever()
